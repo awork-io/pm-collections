e392b9f7306297e16e7564776264e51c77f4bbc0
atedesch1 <alvaro_porc@hotmail.com>
2022-03-22 18:20:22 -0300

    implement QueryParam class

--- lib/classes/QueryParam.ts
+++ lib/classes/QueryParam.ts
@@ -1,1 +1,25 @@
-export class QueryParam {}
+import { IDescription } from "../interfaces/description-interface";
+import { IKeyValue } from "../interfaces/key-value-interface";
+import { Description } from "./Description";
+
+export class QueryParam implements IKeyValue {
+    key: string;
+    value: string;
+    disabled?: boolean | undefined;
+    description?: string | IDescription | Description | undefined;
+
+    constructor(options: IKeyValue) {
+        this.key = options.key;
+        this.value = options.value;
+        this.disabled = options.disabled;
+        this.description = Description.resolve(options.description);
+    }
+
+    static resolve(obj: any) {
+        return this.isQueryParam(obj) ? new QueryParam(obj) : obj;
+    }
+
+    static isQueryParam(obj: any) {
+        return obj.key && obj.value;
+    } 
+}
