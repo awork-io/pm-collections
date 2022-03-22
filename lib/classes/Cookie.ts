37573605d0c185354426de414f7014a9e6dcf596
atedesch1 <alvaro_porc@hotmail.com>
2022-03-22 19:03:59 -0300

    implement Cookie class

--- lib/classes/Cookie.ts
+++ lib/classes/Cookie.ts
@@ -1,1 +1,48 @@
-export class Cookie {}
+import { ICookie } from "../interfaces/cookie-interface";
+
+export class Cookie implements ICookie {
+    domain: string;
+    expires?: string | undefined;
+    maxAge?: string | undefined;
+    hostOnly?: boolean | undefined;
+    httpOnly?: boolean | undefined;
+    name?: string | undefined;
+    path: string;
+    secure?: boolean | undefined;
+    session?: boolean | undefined;
+    value?: string | undefined;
+    extensions?: any;
+
+    constructor(options: ICookie) {
+        this.domain = options.domain;
+        this.expires = options.expires;
+        this.maxAge = options.maxAge;
+        this.hostOnly = options.hostOnly;
+        this.httpOnly = options.httpOnly;
+        this.name = options.name;
+        this.path = options.path;
+        this.secure = options.secure;
+        this.session = options.session;
+        this.value = options.value;
+        this.extensions = options.extensions;
+    }
+
+    static resolve(obj: any) {
+        if (obj instanceof Cookie || !this.validate(obj))
+            return obj;
+        return new Cookie(obj);
+    }
+    static validate(obj: any) {
+        return obj.domain ||
+            obj.expires ||
+            obj.maxAge ||
+            obj.hostOnly ||
+            obj.httpOnly ||
+            obj.name || 
+            obj.path ||
+            obj.secure ||
+            obj.session ||
+            obj.value ||
+            obj.extensions;
+    } 
+}
