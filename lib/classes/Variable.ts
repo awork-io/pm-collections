3f69e3845f82f7eaeba65420a071dfc7045dcb69
atedesch1 <alvaro_porc@hotmail.com>
2022-03-22 18:50:14 -0300

    implement resolve and validate methods for Variable

--- lib/classes/Variable.ts
+++ lib/classes/Variable.ts
@@ -12,10 +12,28 @@ export class Variable implements IVariable {
         this.type = options.type;
         this.system = options.system;
     }
+
+    static resolve(obj: any) {
+        if (obj instanceof Variable || !this.validate(obj))
+            return obj;
+        return new Variable(obj);
+    }
+    static validate(obj: any) {
+        return obj.key || obj.value || obj.type || obj.system;
+    }
 }
 
 export class VariableList extends PropertyList<Variable> {
     constructor(list: IVariable[] | Variable[]) {
         super(list.map(variableLike => new Variable(variableLike)));
+    }
+
+    static resolve(arr: any) {
+        if (arr instanceof VariableList || !this.validate(arr))
+            return arr;
+        return new VariableList(arr);
+    }
+    static validate(arr: any) {
+        return arr.every((obj: any) => Variable.validate(obj));
     }
 }
