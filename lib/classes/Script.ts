0ba0b432b3fde6f9f9e04024f494b7bd6e0995ba
atedesch1 <alvaro_porc@hotmail.com>
2022-03-22 18:54:47 -0300

    implement Script class

--- lib/classes/Script.ts
+++ lib/classes/Script.ts
@@ -1,1 +1,28 @@
-export class Script {}
+import { IScript } from "../interfaces/script-interface";
+import { IUrl } from "../interfaces/url-interface";
+import { Url } from "./Url";
+
+export class Script implements IScript {
+    id?: string;
+    type?: string;
+    exec?: string | string[];
+    src?: string | IUrl | Url;
+    name?: string;
+
+    constructor(options: IScript) {
+        this.id = options.id;
+        this.type = options.type;
+        this.exec = options.exec;
+        this.src = Url.resolve(options.src);
+        this.name = options.name;
+    }
+
+    static resolve(obj: any) {
+        if (obj instanceof Script || !this.validate(obj))
+            return obj;
+        return new Script(obj);
+    }
+    static validate(obj: any) {
+        return obj.id || obj.type || obj.exec || obj.src || obj.name;
+    }
+}
