caea26a8a4e2086975261fa496a10b540301f51b
atedesch1 <alvaro_porc@hotmail.com>
2022-03-22 19:10:08 -0300

    fix response interface

--- lib/interfaces/response-interface.ts
+++ lib/interfaces/response-interface.ts
@@ -2,7 +2,7 @@ import { Cookie } from "../classes/Cookie";
 import { Header, HeaderList } from "../classes/Header";
 import { IBody } from "./body-interface";
 import { ICookie } from "./cookie-interface";
-import { IHeader } from "./header-interface";
+import { IKeyValue } from "./key-value-interface";
 import { IRequest } from "./request-interface";
 
 export interface IResponse {
@@ -10,7 +10,7 @@ export interface IResponse {
     originalRequest?: IRequest | Request;
     responseTime?: string | number;
     timings?: any;
-    header?: IHeader[] | Header[] | HeaderList[];
+    header?: IKeyValue[] | Header[] | HeaderList[];
     cookie?: ICookie[] | Cookie[];
     body?: IBody;
     status?: string;
