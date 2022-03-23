1ecf94670a1b1d70f44563f0c3ce34e706c559c9
atedesch1 <alvaro_porc@hotmail.com>
2022-03-23 14:01:22 -0300

    add classes exports

---
+++ lib/index.ts
@@ -0,0 +1,38 @@
+import { Auth } from "./classes/Auth";
+import { Collection } from "./classes/Collection";
+import { Cookie } from "./classes/Cookie";
+import { Description } from "./classes/Description";
+import { Header, HeaderList } from "./classes/Header";
+import { Information } from "./classes/Information";
+import { Folder, Item } from "./classes/Item";
+import { PropertyList } from "./classes/PropertyList";
+import { QueryParam } from "./classes/QueryParam";
+import { Request } from "./classes/Request";
+import { Response } from "./classes/Response";
+import { Script } from "./classes/Script";
+import { Url } from "./classes/Url";
+import { Body } from "./classes/Body";
+import { Event } from "./classes/Event";
+import { Variable, VariableList } from "./classes/Variable";
+
+export {
+    Auth,
+    Body,
+    Collection,
+    Cookie,
+    Description,
+    Event,
+    Header,
+    HeaderList,
+    Information,
+    Item,
+    Folder,
+    PropertyList,
+    QueryParam,
+    Request,
+    Response,
+    Script,
+    Url,
+    Variable,
+    VariableList
+};
