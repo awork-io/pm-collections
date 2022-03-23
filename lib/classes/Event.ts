819360e0dccdf58c3d7a8d260cdd422282dc6801
atedesch1 <alvaro_porc@hotmail.com>
2022-03-23 13:48:16 -0300

    implement Event class

--- lib/classes/Event.ts
+++ lib/classes/Event.ts
@@ -1,2 +1,52 @@
-export class Event {}
-export class EventList {}
+import { IDescription } from "../interfaces/description-interface";
+import { IEvent } from "../interfaces/event-interface";
+import { IScript } from "../interfaces/script-interface";
+import { Description } from "./Description";
+import { PropertyList } from "./PropertyList";
+import { Script } from "./Script";
+
+export class Event implements IEvent {
+    listen: string;
+    script?: IScript | Script | undefined;
+    id?: string | undefined;
+    name?: string | undefined;
+    disabled?: boolean | undefined;
+    description?: string | IDescription | Description | undefined;
+
+    constructor(options: IEvent) {
+        this.listen = options.listen;
+        this.script = Script.resolve(options.script);
+        this.id = options.id;
+        this.name = options.name;
+        this.disabled = options.disabled;
+        this.description = Description.resolve(options.description);
+    }
+
+    static resolve(obj: any) {
+        if (obj instanceof Event || !this.validate(obj))
+            return obj;
+        return new Event(obj);
+    }
+    static validate(obj: any) {
+        return obj.listen ||
+            obj.script ||
+            obj.id ||
+            obj.name ||
+            obj.disabled ||
+            obj.description;
+    } 
+}
+export class EventList extends PropertyList<Event> {
+    constructor(list: IEvent[] | Event[]) {
+        super(list.map(eventLike => Event.resolve(eventLike)));
+    }
+
+    static resolve(arr: any) {
+        if (arr instanceof EventList || !this.validate(arr))
+            return arr;
+        return new EventList(arr);
+    }
+    static validate(arr: any) {
+        return arr.every((obj: any) => Event.validate(obj));
+    }
+}
