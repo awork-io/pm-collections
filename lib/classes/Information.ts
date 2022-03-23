b5022df65315c5df6733c81c5d2054682795093a
atedesch1 <alvaro_porc@hotmail.com>
2022-03-23 14:35:06 -0300

    add default schema

--- lib/classes/Information.ts
+++ lib/classes/Information.ts
@@ -2,9 +2,11 @@ import { IDescription } from "../interfaces/description-interface";
 import { IInformation } from "../interfaces/information-interface";
 import { Description } from "./Description";
 
+const defSchema = "https://schema.getpostman.com/json/draft-07/collection/v2.1.0/";
+
 export class Information implements IInformation {
     version?: string | { major: number; minor: number; patch: number; identifier?: string | undefined; meta?: any; } | undefined;
-    schema: string;
+    schema?: string;
     id?: string | undefined;
     name?: string | undefined;
     disabled?: boolean | undefined;
@@ -12,7 +14,7 @@ export class Information implements IInformation {
 
     constructor(options: IInformation) {
         this.version = options.version;
-        this.schema = options.schema;
+        this.schema = options.schema || defSchema;
         this.id = options.id;
         this.name = options.name;
         this.disabled = options.disabled;
