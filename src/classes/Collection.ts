ca1efb88957fd8deba9627c3ed961c183c13b636
atedesch1 <alvaro_porc@hotmail.com>
2022-03-23 16:44:56 -0300

    collection now moves id, name and description into info field

--- src/classes/Collection.ts
+++ src/classes/Collection.ts
@@ -1,28 +1,31 @@
 import { ICollection } from "../types/collection-interface";
-import { IDescription } from "../types/description-interface";
 import { IFolder } from "../types/folder-interface";
 import { IInformation } from "../types/information-interface";
 import { IItem } from "../types/item-interface";
 import { Auth } from "./Auth";
 import { Description } from "./Description";
 import { EventList, Event } from "./Event";
+import { Information } from "./Information";
 import { Folder, Item } from "./Item";
 import { VariableList, Variable } from "./Variable";
 
 export class Collection implements ICollection {
-    info?: IInformation;
+    info?: IInformation | Information;
     protocolProfileBehavior?: any;
     variable?: VariableList | Variable[] | undefined;
     item: (IFolder | IItem | Item | Folder)[];
     event?: EventList | Event[] | undefined;
     auth?: Auth | undefined;
     id?: string | undefined;
-    name?: string | undefined;
     disabled?: boolean | undefined;
-    description?: string | IDescription | Description | undefined;
 
     constructor(options: ICollection) {
-        this.info = options.info;
+        this.info = new Information({
+            ...options.info,
+            id: options.id, 
+            name: options.name,
+            description: Description.resolve(options.description),
+        });
         this.protocolProfileBehavior = options.protocolProfileBehavior;
         this.variable = VariableList.resolve(options.variable);
         this.item = options.item.map((i: any) =>
@@ -32,10 +35,7 @@ export class Collection implements ICollection {
         );
         this.event = EventList.resolve(options.event);
         this.auth = options.auth;
-        this.id = options.id;
-        this.name = options.name;
         this.disabled = options.disabled;
-        this.description = Description.resolve(options.description);
     }
     static resolve(obj: any) {
         if (obj instanceof Collection || !this.validate(obj))
