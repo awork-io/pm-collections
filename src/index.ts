cb4b1b03d1cd5ed202e901da778b96a592add3e6
atedesch1 <alvaro_porc@hotmail.com>
2022-03-24 19:09:18 -0300

    update build files

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,37 @@
-import { Auth } from "./classes/Auth";
-import { Collection } from "./classes/Collection";
-import { Cookie } from "./classes/Cookie";
-import { Description } from "./classes/Description";
-import { Header, HeaderList } from "./classes/Header";
-import { Information } from "./classes/Information";
-import { Folder, Item } from "./classes/Item";
-import { PropertyList } from "./classes/PropertyList";
-import { QueryParam } from "./classes/QueryParam";
-import { Request } from "./classes/Request";
-import { Response } from "./classes/Response";
-import { Script } from "./classes/Script";
-import { Url } from "./classes/Url";
-import { Body } from "./classes/Body";
-import { Event } from "./classes/Event";
-import { Variable, VariableList } from "./classes/Variable";
+export * from "./types/auth-interface";
+export * from "./types/base-property-interface";
+export * from "./types/body-interface";
+export * from "./types/certificate-interface";
+export * from "./types/collection-interface";
+export * from "./types/cookie-interface";
+export * from "./types/description-interface";
+export * from "./types/event-interface";
+export * from "./types/folder-interface";
+export * from "./types/information-interface";
+export * from "./types/item-interface";
+export * from "./types/key-value-interface";
+export * from "./types/property-interface";
+export * from "./types/property-list-interface";
+export * from "./types/proxy-config-interface";
+export * from "./types/request-interface";
+export * from "./types/response-interface";
+export * from "./types/script-interface";
+export * from "./types/url-interface";
+export * from "./types/variable-interface";
 
-export {
-  Auth,
-  Body,
-  Collection,
-  Cookie,
-  Description,
-  Event,
-  Header,
-  HeaderList,
-  Information,
-  Item,
-  Folder,
-  PropertyList,
-  QueryParam,
-  Request,
-  Response,
-  Script,
-  Url,
-  Variable,
-  VariableList,
-};
+export * from "./classes/Auth";
+export * from "./classes/Body";
+export * from "./classes/Collection";
+export * from "./classes/Cookie";
+export * from "./classes/Description";
+export * from "./classes/Event";
+export * from "./classes/Header";
+export * from "./classes/Information";
+export * from "./classes/Item";
+export * from "./classes/PropertyList";
+export * from "./classes/QueryParam";
+export * from "./classes/Request";
+export * from "./classes/Response";
+export * from "./classes/Script";
+export * from "./classes/Url";
+export * from "./classes/Variable";
