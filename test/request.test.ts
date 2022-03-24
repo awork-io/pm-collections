30669711618de248e80b3bf15df6a8249f83dd1b
atedesch1 <alvaro_porc@hotmail.com>
2022-03-24 19:36:26 -0300

    add useless test

---
+++ test/request.test.ts
@@ -0,0 +1,12 @@
+import { Request } from "../src";
+
+test("Request creation", () => {
+    const request = new Request({
+        description: "any description",
+        url: "new url"
+    });
+    expect(JSON.parse(JSON.stringify(request))).toStrictEqual({
+        description: "any description",
+        url: "new url"
+    });
+});
